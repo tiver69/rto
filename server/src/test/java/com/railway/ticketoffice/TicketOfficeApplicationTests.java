8915b23f4de36e66bc42f38e870afa350cd074e7
Aleksandra <kibko.sasha@gmail.com>
2019-08-10 20:29:29 +0300

    init server spring boot app

---
+++ server/src/test/java/com/railway/ticketoffice/TicketOfficeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.railway.ticketoffice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TicketOfficeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
